a2adbdde9304845e16eb2e5fdb8e4ce2a7215853
vivian wambugu <ythirav@gmail.com>
2025-08-20 16:18:18 +0300

    Modified server.js

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const app = express();
 app.use(express.json());
 
 const hostname = '127.0.0.1';
-const port = process.env.PORT;
+const port = process.env.PORT
 
-  app.listen ('/' , (req, res) => {
-    res.send('Welcome to Durja!');
-  });
+app.get('/', (req, res) => {
+  res.send("Welcome to Durja!");
+})
+
   app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
   });
