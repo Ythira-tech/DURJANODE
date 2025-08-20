50d570111c70e5fcc62fcc12c3821904eedfbb92
vivian wambugu <ythirav@gmail.com>
2025-08-20 15:38:27 +0300

    Added all files

---
+++ server.js
@@ -0,0 +1,19 @@
+ require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT;
+
+app.get('/', (req, res)) => {
+  res.send('Welcome to Durja!');
+  app.listen ('/' , (req, res) => {
+    res.send('Welcome to Durja!');
+  });
+  app.listen(port, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+}
